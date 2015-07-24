44cd98f3b2255fc86b16704e62d59115553dab8c
Glenn Bitar <glennbitar@gmail.com>
2015-07-24 06:53:08 -0700

    Using peewee queries to check for existing tables and values. Hopefully this will work.

--- database.py
+++ database.py
@@ -1,6 +1,4 @@
-import peewee
 from peewee import *
-
 import urllib.parse
 import os
 
@@ -9,18 +7,47 @@ dbname = url.path
 db = MySQLDatabase(dbname, host=url.hostname, user=url.username, passwd=url.password)
 
 
-class BaseModel(Model):
-    """A base model for all other models"""
+class Setting(Model):
+    name = CharField(index=True, unique=True)
+    value = FloatField()
 
     class Meta:
         database = db
 
 
-class Setting(BaseModel):
-    Name = CharField(index=True, unique=True)
-    Value = FloatField()
+db.connect()
 
 
-counter_1 = Setting()
-counter_1.Name = 'Counter 1'
-counter_1.Value = 0
+# Create setting table if it doesn't exist already
+if 'setting' not in db.get_tables():
+    db.create_table(Setting)
+    print('Setting table not found. Created.')
+else:
+    print('Setting table exists. Do nothing.')
+
+# Delete if true
+if True:
+    print('Deleting all entries in table Setting')
+    for setting in Setting.select():
+        setting.delete_instance()
+
+# Define function for safely creating setting
+def create_safe_setting(name, value):
+    setting = Setting()
+    try:
+        print('Trying to find ' + name + '...')
+        setting = Setting.get(Setting.name == name)
+        print('Found it! Assigning...')
+    except DoesNotExist:
+        print(name + ' not found. Creating...')
+        setting = Setting.create(name=name, value=value)
+        setting.save()
+    return setting
+
+# Create setting_1 if it doesn't exist already
+setting_1 = create_safe_setting('setting_1', 0)
+setting_2 = create_safe_setting('setting_2', 12.3)
+
+print()
+print(setting_1.name, ':', setting_1.value)
+print(setting_2.name, ':', setting_2.value)
--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import database
 
 while True:
     print('Counter value at start:')
-    print(database.counter_1.Value)
+    print(database.setting_1.Value)
 
     print('Increase counter value by 3:')
-    database.counter_1.Value += 3
+    database.setting_1.Value += 3
+    database.setting_1.save()
     print(database.counter_1.Value)
 
     print('Going to sleep for 10 seconds...')
