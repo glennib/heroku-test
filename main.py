b29602905ad0d7bc734af49556ad43df36c9ca60
Glenn Bitar <glennbitar@gmail.com>
2015-07-24 07:12:31 -0700

    counter_1 to setting_1

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ while True:
     print('Increase counter value by 3:')
     database.setting_1.value += 3
     database.setting_1.save()
-    print(database.counter_1.value)
+    print(database.setting_1.value)
 
     print('Going to sleep for 10 seconds...')
 
