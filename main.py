6ff1c5ee98ab0c73516c7d6cd2b44cab3061bb30
Glenn Bitar <glennbitar@gmail.com>
2015-07-24 07:11:07 -0700

    More of the same. Missed a couple of entries.

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import database
 
 while True:
     print('Counter value at start:')
-    print(database.setting_1.Value)
+    print(database.setting_1.value)
 
     print('Increase counter value by 3:')
     database.setting_1.value += 3
     database.setting_1.save()
-    print(database.counter_1.Value)
+    print(database.counter_1.value)
 
     print('Going to sleep for 10 seconds...')
 
